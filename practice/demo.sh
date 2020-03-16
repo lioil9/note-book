4d408f8e777062f875a065ac3a562acb35f45d78
lioil9 <156949416@qq.com>
2020-03-17 01:02:16 +0800

    update

---
+++ practice/demo.sh
@@ -0,0 +1,10 @@
+mkdir ./example
+mv ./ex01.c ./example/example.c
+mkdir ./practice
+mv ./ex02.c ./practice/practice.c
+rm ./ex03.c
+touch README.md
+echo "# example/example.c  
+判断是奇数还是偶数  
+# practice/practice.c  
+比较两个数大小  " >> ./README.md
